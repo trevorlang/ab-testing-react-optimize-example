c81056953a7f9f2ee026e337bf5af85bb58a67b5
Trevor Lang <trevorlang@ymail.com>
2022-12-15 20:30:25 -0700

    Adding google tag manager

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import eslint from 'vite-plugin-eslint'
+import { VitePluginRadar } from 'vite-plugin-radar'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    eslint()
-  ]
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  console.log('VitePluginRadar', VitePluginRadar)
+  console.log('env.VITE_GTM_CONTAINER_ID', env.VITE_GTM_CONTAINER_ID)
+  return {
+    plugins: [
+      react(),
+      eslint(),
+      VitePluginRadar({
+        enableDev: true,
+        // Google Analytics tag injection
+        gtm: {
+          id: env.VITE_GTM_CONTAINER_ID
+        }
+      })
+    ]
+  }
 })
